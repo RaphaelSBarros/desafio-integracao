68b2f5e7d3ade2e045ba881bd4a3360eb3c55f11
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-07-18 19:16:53 -0300

    Ajuste no Json

--- fashion.js
+++ fashion.js
@@ -4,7 +4,7 @@ let fashionJson = [
     {id: 3, name: 'Jaqueta', img: 'images/roupa2.webp', price: 59, sizes: ['S', 'M', 'L'], description: 'Jaqueta Venture 2 Preta Estilosa'},
     {id: 4, name: 'Calça Jeans', img: 'images/roupa3.webp', price: 97.99, sizes: ['S', 'M', 'L'], description: 'Luxe Sport Paxtyn with Clean Pocket in Delos'},
     {id: 5, name: 'Jaqueta Resolve', img: 'images/roupa4.webp', price: 26.99, sizes: ['S', 'M', 'L'], description: 'Jaqueta Resolve Insulated'},
-    {id: 6, name: 'Camisola?', img: 'images/roupa5.webp', price: 59.99, sizes: ['S', 'M', 'L'], description: 'Cross-front Cami in soft white/black'},
+    {id: 6, name: 'Blusa', img: 'images/roupa5.webp', price: 59.99, sizes: ['S', 'M', 'L'], description: 'Cross-front Cami in soft white/black'},
     {id: 7, name: 'Camisa', img: 'images/roupa6.webp', price: 95.90, sizes: ['S', 'M', 'L'], description: 'Camiseta Levis The Perfect'},
     {id: 8, name: 'Regata', img: 'images/roupa7.webp', price: 29.90, sizes: ['S', 'M', 'L'], description: 'Top Regata Básica'}
 ]
